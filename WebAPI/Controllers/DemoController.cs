e4a0eeac14336020fcb582ee799b92fb2f6bc994
Abel Berhane <aberhane@datadimensions.com>
2017-08-16 11:29:38 -0400

    First Web API commit

---
+++ WebAPI/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get()
+        {
+            return "Welcome To My First Web API";   
+        }
+        
+        public List < string > Get(int Id)
+        {
+            return new List<string>{
+
+                "Data1",
+                "Data2"
+            };
+        }
+
+
+
+
+    }
+}
